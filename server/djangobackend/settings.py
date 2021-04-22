4adba00ee35ea1762c33957ab57402949813815f
lucachiossi <engr.luca.chiossi@gmail.com>
2021-04-22 16:56:21 +0100

    added functionality in settings to load environment variables from file -> for restapis calls

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -11,6 +11,15 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+# Personal keys
+load_dotenv()
+env_path = Path('.')/'.env'
+load_dotenv(dotenv_path=env_path)
+
+CP_API_KEY = os.getenv("CP_API_KEY")
+CP_API_URL = os.getenv("CP_API_URL")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
