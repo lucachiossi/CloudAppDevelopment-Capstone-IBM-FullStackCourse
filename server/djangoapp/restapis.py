eee0456cbd6c8020020300483fe6ff4d921d4796
lucachiossi <engr.luca.chiossi@gmail.com>
2021-04-22 15:20:31 +0100

    implemented get_request function to retrieve information

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,12 +1,37 @@
 import requests
 import json
-# import related models here
+from .models import CarDealer
 from requests.auth import HTTPBasicAuth
 
 
-# Create a `get_request` to make HTTP GET requests
-# e.g., response = requests.get(url, params=params, headers={'Content-Type': 'application/json'},
-#                                     auth=HTTPBasicAuth('apikey', api_key))
+def get_request(url, **kwargs):
+    """
+    get_request function will call request.get(url, headers, params=kwargs)
+    the function is expected to work with http messages passing data in json format
+
+    url: url passed as parameter
+    headers: 'Content-Type' and 'cp_api_key'
+    params: pairs passed as parameters (kwargs)
+
+    'cp_api_key' needs the environment variable $CP_API_KEY defined with valid key to access
+        the rest api
+    'Content-Type' value fixed as 'application/json'
+    """
+    cp_api_key = ""
+    headers = {'Content-Type': 'application/json', 'cp_api_key': cp_api_key}
+    try:
+        # call get method with loaded payload
+        response = requests.get(url,headers=headers,params=kwargs)
+    except:
+        # if any error occurs print it
+        print("Network exception occurred!!!")
+        return {}
+    status_code = response.status_code
+    print("received response with status code {}".format(status_code))
+    json_data = json.loads(response.text)
+    return json_data
+
+
 
 
 # Create a `post_request` to make HTTP POST requests
@@ -29,6 +54,3 @@ from requests.auth import HTTPBasicAuth
 # def analyze_review_sentiments(text):
 # - Call get_request() with specified arguments
 # - Get the returned sentiment label such as Positive or Negative
-
-
-
