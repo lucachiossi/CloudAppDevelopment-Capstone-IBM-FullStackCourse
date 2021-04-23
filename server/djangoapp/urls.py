fbbfc0c4d064017fde14fc664740bd2ace9af2e1
lucachiossi <engr.luca.chiossi@gmail.com>
2021-04-23 16:42:00 +0100

    added add_review url to urls

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -28,8 +28,9 @@ urlpatterns = [
     path('logout/', view=views.logout_request, name='logout'),
 
     # path for dealer reviews view
-    path('dealer/<int:dealer_id>/', view=views.get_dealer_details, name='dealer_details')
+    path('dealer/<int:dealer_id>/', view=views.get_dealer_details, name='dealer_details'),
 
     # path for add a review view
+    path('add_review/', view=views.add_review, name='add_review')
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
