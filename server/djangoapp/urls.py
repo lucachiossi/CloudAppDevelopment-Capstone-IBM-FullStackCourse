4f143e0a07dec6053e4faf552f96293d0eb49bf0
lucachiossi <engr.luca.chiossi@gmail.com>
2021-04-23 11:06:09 +0100

    added dealer_details url to urls

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     # route is a string contains a URL pattern
     # view refers to the view function
     # name the URL
+
+    # path for dealerships view
     path(route='', view=views.get_dealerships, name='index'),
 
     # path for about view
@@ -26,6 +28,7 @@ urlpatterns = [
     path('logout/', view=views.logout_request, name='logout'),
 
     # path for dealer reviews view
+    path('dealer/<int:dealer_id>/', view=views.get_dealer_details, name='dealer_details')
 
     # path for add a review view
 
